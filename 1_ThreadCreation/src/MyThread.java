96f375f46d48eec54d4b6f1770b40cc24661599f
RupamThakre <rupamthakre15@gmail.com>
2025-07-10 18:26:46 +0530

    ways to create multithreaded

---
+++ 1_ThreadCreation/src/MyThread.java
@@ -0,0 +1,10 @@
+/*
+* 1. By extending MyThread
+* 2. By implementing Runnable
+* */
+
+public class MyThread extends Thread {
+    public void run(){
+        System.out.println("Thread is running ......");
+    }
+}
